920d7c6ddb050a2162cad2ea0f7e01fb9f5128ad
mishastik78 <69627188+mishastik78@users.noreply.github.com>
2023-03-02 20:11:26 +0300

    model history added

--- adminpanel/promo/models.py
+++ adminpanel/promo/models.py
@@ -18,10 +18,18 @@ class IdTimeMixin(models.Model):
 class Product(IdTimeMixin):
     name = models.CharField('Наименование продукта', max_length=255)
 
+    class Meta:
+        db_table = 'content\".\"product'
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
 
-class ProductThrough(IdTimeMixin):
+
+class ProductPromoCode(IdTimeMixin):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    promocode = models.ForeignKey('PromoCode', on_delete=models.CASCADE)
+    promo_code = models.ForeignKey('PromoCode', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'content\".\"product_promo'
 
 
 class PromoCode(IdTimeMixin):
@@ -32,13 +40,33 @@ class PromoCode(IdTimeMixin):
 
     title = models.CharField('Название', max_length=255, blank=True, null=True)
     description = models.TextField('Описание', blank=True, null=True)
-    code = models.CharField('Код', max_length=50)
+    code = models.CharField('Код', max_length=50, unique=True, db_index=True)
     start_at = models.DateTimeField('Код действителен с')
     expired = models.DateTimeField('Код действителен до')
     user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='promos',
                                 verbose_name='Код привязан к пользователю', blank=True, null=True)
     product_ids = models.ManyToManyField(Product, related_name='promos', verbose_name='Код привязан к продуктам',
-                                         through=ProductThrough, blank=True)
+                                         through=ProductPromoCode, blank=True)
     activates_possible = models.PositiveIntegerField('Количество возможных активаций')
     activates_left = models.PositiveIntegerField('Количество оставшихся активаций')
     discount_type = models.CharField('Тип лояльности', max_length=30, choices=DiscountType.choices)
+    discount_amount = models.DecimalField('Размер скидки/фикса', decimal_places=2)
+
+    class Meta:
+        db_table = 'content\".\"promo'
+        verbose_name = 'промокод'
+        verbose_name_plural = 'промокоды'
+
+
+class History(IdTimeMixin):
+    promocode = models.ForeignKey(PromoCode, on_delete=models.PROTECT, related_name='history',
+                                  verbose_name='Активированный промокод')
+    applied_user_id = models.ForeignKey(User, on_delete=models.PROTECT, related_name='history',
+                                        verbose_name='Промокод применен к пользователю')
+    discount_amount = models.DecimalField('Размер примененной скидки в валюте счета', decimal_places=2, editable=False)
+    billing_info = models.TextField('Информация от сервиса оплаты')
+
+    class Meta:
+        db_table = 'content\".\"history'
+        verbose_name = 'история активации кода'
+        verbose_name_plural = 'история активаций кодов'
